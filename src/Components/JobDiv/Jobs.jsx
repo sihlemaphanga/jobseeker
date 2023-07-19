3d79b8123d1a91137f763836f64b38dfc6807ded
sihlemaphanga <sihlemaphangass@gmail.com>
2023-07-19 11:42:36 +0200

    added the jobs card

--- src/Components/JobDiv/Jobs.jsx
+++ src/Components/JobDiv/Jobs.jsx
@@ -4,7 +4,18 @@ import React from 'react'
 const Jobs = () => {
   return (
     <div>
-      job
+     <div className="jobsContainer flex gap justify center flex-wrap items-center 
+     py-10" >
+
+      <div className='group/item singleJob w-[250px] p-[20px] bg-white rounded
+      [10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg'>
+
+    <span className='flex justify-between items-center gap-4'>
+    <h1 className='text-[15px] font-semibold text-text color group-hover:text-white'>Web Developer</h1>
+    </span>
+      </div>
+    
+     </div>
     </div>
   )
 }
